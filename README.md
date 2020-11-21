883fc034b7b2eb428b9b68f4e535cf1a12e8a485
chukwudinweze <chukwudinwez@gmail.com>
2020-11-21 13:47:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# freecodecamp-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/freecodecamp-react)
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Greeting() {
+  return <h1>I'm Chukwudi and Building my first component</h1>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
