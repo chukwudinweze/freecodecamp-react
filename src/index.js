bf59f799add24b2ff3fa7d36025c9a0111227554
chukwudinweze <chukwudinwez@gmail.com>
2020-11-21 14:39:40 +0100

    nested components in react

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom";
 function Greeting() {
   return (
     <div>
-     <Person/>
-      <h3>My name is Chukwudi Nweze</h3>
+      <Person />
+      <Message />
     </div>
   );
 }
-const Person = () => <h1>hello world</h1>
+const Person = () => <h1>hello world</h1>;
+const Message = () => {
+  return <p>hello world</p>;
+};
 
 ReactDOM.render(<Greeting />, document.getElementById("root"));
