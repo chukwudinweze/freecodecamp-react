5cc5ee63036c87a31a85d700ffa9b807b0e438b8
chukwudinweze <chukwudinwez@gmail.com>
2020-11-21 15:07:05 +0100

    created my first react project  A book list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Greeting() {
+function Booklist() {
+ 
   return (
     <div>
-      <Person />
-      <Message />
+      <Book  />
+      <Book  />
+      <Book  />
+      <Book  />
+      <Book  />
     </div>
   );
 }
-const Person = () => <h1>hello world</h1>;
-const Message = () => {
-  return <p>hello world</p>;
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg"
+      alt="bestseller"
+    />
+  );
 };
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Title = () => {
+  return <h3>A Promised Land</h3>;
+};
+
+const Author = () => {
+  return <h2>Barack Obama</h2>;
+};
+
+ReactDOM.render(<Booklist />, document.getElementById("root"));
