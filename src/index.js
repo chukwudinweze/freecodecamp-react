88d2436b872e9e1ca17fcdb423a261a8c0ac0ea9
chukwudinweze <chukwudinwez@gmail.com>
2020-11-22 13:15:35 +0100

    learnt about iterating through array of objects and mapping it to componnts

--- src/index.js
+++ src/index.js
@@ -1,44 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
-let firstBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
-  title: "A Promised Land",
-  author: "Barack Obama"
-};
 
-let secondBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg",
-  title: "Greenlights",
-  author: "Matthew McConaughey"
-};
+let Books = [
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
+    title: "A Promised Land",
+    author: "Barack Obama"
+  },
+
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg",
+    title: "Greenlights",
+    author: "Matthew McConaughey"
+  },
+   {img:
+      "https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL._AC_UL200_SR200,200_.jpg",
+    title: "Becoming",
+    author: "Michelle Obama"
+  },
+];
+
 function Booklist() {
   return (
-    <div className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      >
-        here is the children and i am still learning
-      </Book>
-    </div>
+    <section className="booklist">
+      {Books.map(book => {
+        // const { img, title, author } = book;
+        return (
+          <div>
+            <Book book={book} />
+          </div>
+        );
+      })}
+    </section>
   );
 }
-const Book = ({ img, title, author, children }) => {
+const Book = props => {
+  const { img, title, author } = props.book;
   return (
     <article className="book">
       <img src={img} />
       <h1>{title}</h1>
       <h1>{author}</h1>
-      <p>{children}</p>
     </article>
   );
 };
